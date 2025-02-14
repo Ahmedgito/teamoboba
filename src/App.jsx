54402e1224ddd385f810b333dcdbfe2c83b1acef
Ahmedgito <mamooabid1@gmail.com>
2025-02-14 23:44:36 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='mt-10 text-3xl text-center'>HELLO MY NAME IS AHMED</h1>
+    </>
+  )
+}
+
+export default App
