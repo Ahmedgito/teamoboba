5d65b76e721362892ce79ea0022e20594e17b9a1
Ahmedgito <mamooabid1@gmail.com>
2025-02-15 06:53:10 +0500

    feat: improve responsiveness of Order Now button in Section1 component

--- src/Section1.jsx
+++ src/Section1.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import header from "./assets/header.png";
-import Button from "./components/Button";
 
 const Section1 = () => {
   return (
@@ -8,11 +7,11 @@ const Section1 = () => {
       {/* Header Image */}
       <img src={header} alt="Header" className="w-full h-auto md:w-[1100px] md:h-[600px]" />
 
-      {/* Order Now Button - Positioned at Bottom Left */}
-      <div className="absolute bottom-28 left-6">
-      <button className="bg-[#4b2b1c] text-white font-bold py-3 px-6 rounded-4xl shadow-lg hover:bg-[#A0522D] transition-all">
-        Order Now
-      </button>
+      {/* Order Now Button - Responsive Placement */}
+      <div className="absolute bottom-0 left-1/4 transform -translate-x-1/2 md:bottom-28 md:left-6 md:translate-x-0">
+        <button className="bg-[#4b2b1c] text-white font-bold md:py-3 py-2 px-4 md:px-6 rounded-full shadow-lg hover:bg-[#A0522D] transition-all">
+          Order Now
+        </button>
       </div>
     </div>
   );
